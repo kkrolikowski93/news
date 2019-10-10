228c0d1f01b98150043918d940d9dbaeb4e66f07
Kamil Krolikowski <kamil.krolikowski@tvt.biz>
2019-10-10 16:52:40 +0200

    feat: adding required parameter to inputs

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
         header('Location: index.php');
         exit();
     } else {
-        echo 'Wrong credentials';
+        echo '<p style="color:red">Wrong credentials</p>';
     }
 }
 ?>
@@ -29,28 +29,17 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
 <script>
 
-    function submitlogin() {
-        var form = document.login;
-        if (form.email.value == "") {
-            alert("Enter email or username.");
-            return false;
-        }
-        else if (form.password.value == "") {
-            alert("Enter password.");
-            return false;
-        }
-    }
 
 </script>
 <h1>Login Here</h1>
 <form action="" method="post" name="login">
     <label for="email">Email</label>
-    <input type="text" id="email" name="email" placeholder="Enter your email"/>
+    <input type="text" id="email" name="email" required placeholder="Enter your email"/>
 
     <label for="password">Password</label>
-    <input type="password" id="password" name="password" placeholder="Enter your password"/>
-    <input onclick="return(submitlogin());" type="submit" class="submit" name="submit" value="Login"/>
-
+    <input type="password" id="password" name="password" required placeholder="Enter your password"/>
+    <input type="submit" class="submit" name="submit" value="Login"/>
+    <br /><br />
     <a class="submit" href="registration.php">Register</a>
 
 </form>
